411214d9e80a71f1d098b367ce5b4a1ac831ee1f
hyperyuri <yuri.snp@hotmail.com>
2024-04-01 16:10:44 -0300

    remove internal deps

--- contracts/empty/src/empty.rs
+++ contracts/empty/src/empty.rs
@@ -2,9 +2,11 @@
 
 klever_sc::imports!();
 
-/// An empty contract. To be used as a template when starting a new contract from scratch.
+// An empty contract. To be used as a template when starting a new contract from scratch.
 #[klever_sc::contract]
 pub trait EmptyContract {
     #[init]
-    fn init(&self) {}
+    fn init(&self) {
+
+    }
 }
